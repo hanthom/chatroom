fccc222027e5ad7469122d23a5443f37e7848e6c
hanthom <m.hantho@gmail.com>
2015-11-04 14:26:19 -0600

    Tied in getParseData to the controller

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,7 +6,13 @@ app.controller('mainCtrl', function($scope, parseService){
   //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
   //your controllers $scope as messages ($scope.messages)
 
-
+  $scope.getParseData = function() {
+    parseService.getData().then(function(results) {
+      $scope.messages = results.data.results;
+      //console.log($scope.messages);
+    })
+  }
+  $scope.getParseData();
 
   //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postData method on the parseService object which will then post it to the parse backend.
