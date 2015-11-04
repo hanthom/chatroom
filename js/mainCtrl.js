c7b8ce40a8acb20ec8e7f00689814f6eb64edbdb
hanthom <m.hantho@gmail.com>
2015-11-04 15:49:52 -0600

    Updates made to CSS and index

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,10 +6,10 @@ app.controller('mainCtrl', function($scope, parseService){
   //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
   //your controllers $scope as messages ($scope.messages)
 
+
   $scope.getParseData = function() {
     parseService.getData().then(function(results) {
       $scope.messages = results.data.results;
-      //console.log($scope.messages);
     });
   }
   $scope.getParseData();
